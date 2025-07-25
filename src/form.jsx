0badaa86c2eeadcc2796cf7bb90f2d4c9c4e1d77
Rohit <rohitjames77@outlook.com>
2025-07-25 13:17:06 +0530

    Complete personal form

--- src/form.jsx
+++ src/form.jsx
@@ -23,18 +23,18 @@ export default function MainForm() {
   });
 
   const [workExperience, setWorkexp] = useState({
-    companyName: '',
-    position: '',
-    startDate: '',
-    endDate: '',
+    companyName: "",
+    position: "",
+    startDate: "",
+    endDate: "",
   });
 
-  const [summary, setSummary] = useState('');
+  const [summary, setSummary] = useState("");
 
   return (
     <div>
-      <PersonalForm 
-        personalInfo={personalInfo} 
+      <PersonalForm
+        personalInfo={personalInfo}
         setPersonalInfo={setPersonalInfo}
       />
       <Test personalInfo={personalInfo} />
@@ -43,82 +43,66 @@ export default function MainForm() {
 }
 
 function PersonalForm({ personalInfo, setPersonalInfo }) {
-  const handleOnChange = function(event) {
+  const handleOnChange = function (event) {
     const { id, value } = event.target;
-    
-    // Convert id to camelCase property names
-    const fieldMap = {
-      'job-title': 'jobTitle',
-      'first-name': 'firstName',
-      'last-name': 'lastName',
-      'email': 'email',
-      'phone': 'phone',
-      'address': 'address',
-      'city': 'city',
-      'country': 'country'
-    };
-    
-    const fieldName = fieldMap[id];
-    if (fieldName) {
-      setPersonalInfo({ ...personalInfo, [fieldName]: value });
-    }
+    setPersonalInfo({ ...personalInfo, [id]: value });
   };
 
   return (
-    <div className="" id="form-container">
+    <div className="" id="personal-form-container">
       <form className="" id="personal-form">
         <label htmlFor="job-title">Job Title</label>
         <input
           type="text"
           maxLength={50}
           className=""
-          id="job-title"
+          id="jobTitle"
           value={personalInfo.jobTitle}
           onChange={handleOnChange}
           required
         />
-        
+
         <label htmlFor="first-name">First Name</label>
         <input
           type="text"
           maxLength={30}
           className=""
-          id="first-name"
+          id="firstName"
           value={personalInfo.firstName}
           onChange={handleOnChange}
           required
         />
-        
+
         <label htmlFor="last-name">Last Name</label>
         <input
           type="text"
           maxLength={30}
           className=""
-          id="last-name"
+          id="lastName"
           value={personalInfo.lastName}
           onChange={handleOnChange}
           required
         />
-        
+
         <label htmlFor="email">Email</label>
-        <input 
-          type="email" 
-          className="" 
-          id="email" 
+        <input
+          type="email"
+          className=""
+          id="email"
           value={personalInfo.email}
           onChange={handleOnChange}
           required
         />
-        
+
         <label htmlFor="phone">Phone</label>
-        <input 
-          type="tel" 
-          className="" 
-          id="phone" 
+        <input
+          type="tel"
+          className=""
+          id="phone"
           value={personalInfo.phone}
           onChange={handleOnChange}
         />
-        
+
         <label htmlFor="address">Address</label>
         <input
           type="text"
@@ -129,7 +113,7 @@ function PersonalForm({ personalInfo, setPersonalInfo }) {
           onChange={handleOnChange}
           required
         />
-        
+
         <label htmlFor="city">City</label>
         <input
           type="text"
@@ -140,7 +124,7 @@ function PersonalForm({ personalInfo, setPersonalInfo }) {
           onChange={handleOnChange}
           required
         />
-        
+
         <label htmlFor="country">Country</label>
         <input
           type="text"
@@ -155,13 +139,19 @@ function PersonalForm({ personalInfo, setPersonalInfo }) {
     </div>
   );
 }
-
+// Remove this component after use ................................
 function Test({ personalInfo }) {
   return (
     <>
-      <h1 className="text-green-500">
-        Testing: {personalInfo.jobTitle}
-      </h1>
+      <h1 className="text-green-500">Testing: {personalInfo.jobTitle}</h1>
     </>
   );
 }
+
+function EducationForm({ educationInfo, setEducationInfo }) {
+  return( 
+  <div className="" id="education-form-container">
+<button type="button" className="" id="add-education-field"></button>
+  </div>
+
+  )}
