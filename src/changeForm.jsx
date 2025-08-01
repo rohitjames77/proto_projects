805d0b6aefec638e741b22196b0f1a215423f7df
Rohit <rohitjames77@outlook.com>
2025-08-01 10:08:13 +0530

    Add forward/prev button

--- src/changeForm.jsx
+++ src/changeForm.jsx
@@ -1,22 +1,35 @@
+export default function FormForwardButton({ idArr, formId, setFormId }) {
+  const handleOnClick = function () {
+    setFormId((formId + 1));
+  };
+  const buttonPlaceholder = idArr[formId];
+  const handlePrevOnClick = function (){
+    setFormId((formId-1))
+  }
+  if (formId == idArr.length-2) {
+    return (
+      <>
+      <button
+        disabled
+        id="formForwardButton"
+        className="bg-gradient-to-r from-blue-500 to-indigo-400 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 "
+        onClick={handleOnClick}
+      >
+        {"Next: " + buttonPlaceholder}
+      </button>
 
- export default function FormForwardButton ({idArr,formId,setFormId}){
-    // console.log(idArr);
-    // console.log(formId);
- 
+
+      <button id="prevForm" className="" onClick={handlePrevOnClick}  >{"Back"} </button>
     
-const handleOnClick = function (){
-    setFormId((formId+1) % idArr.length);
-       
+    </>
+    );
+  }
+
+  return (
+    <button id="formForwardButton" className="bg-gradient-to-r from-blue-500 to-indigo-400 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 " onClick={handleOnClick}>
+      {"Next: " + buttonPlaceholder}
+    </button>
+  );
 }
 
 
-    return(
-    <button id='nextForm' className="" onClick={handleOnClick}>{formId}</button>
-)
-}
-
-
-
- export function FormBackButton({formId,setFormId}){
-
-}
