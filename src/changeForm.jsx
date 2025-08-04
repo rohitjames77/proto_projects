ed9434ff630723f2583e338c998abedd60600174
Your Name <you@example.com>
2025-08-04 11:45:25 +0530

    Complete forward as back form button timing and appearance logic

--- src/changeForm.jsx
+++ src/changeForm.jsx
@@ -1,35 +1,49 @@
 export default function FormForwardButton({ idArr, formId, setFormId }) {
   const handleOnClick = function () {
-    setFormId((formId + 1));
+    setFormId(formId + 1);
   };
   const buttonPlaceholder = idArr[formId];
-  const handlePrevOnClick = function (){
-    setFormId((formId-1))
-  }
-  if (formId == idArr.length-2) {
+  const handlePrevOnClick = function () {
+    setFormId(formId - 1);
+  };
+
+  if (formId >= 1) {
     return (
       <>
-      <button
-        disabled
-        id="formForwardButton"
-        className="bg-gradient-to-r from-blue-500 to-indigo-400 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 "
-        onClick={handleOnClick}
-      >
-        {"Next: " + buttonPlaceholder}
-      </button>
-
-
-      <button id="prevForm" className="" onClick={handlePrevOnClick}  >{"Back"} </button>
-    
-    </>
+        <button id="prevForm" className="" onClick={handlePrevOnClick}>
+          {"Back"}{" "}
+        </button>
+        <button
+          id="formForwardButton"
+          className="bg-gradient-to-r from-blue-500 to-indigo-400 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 "
+          onClick={handleOnClick}
+          disabled={formId===idArr.length-1}
+        >
+          {"Next: " + buttonPlaceholder}
+          
+        </button>
+      </>
     );
-  }
-
-  return (
-    <button id="formForwardButton" className="bg-gradient-to-r from-blue-500 to-indigo-400 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 " onClick={handleOnClick}>
+  } 
+   
+return (
+    <button
+      id="formForwardButton"
+      className="bg-gradient-to-r from-blue-500 to-indigo-400 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 "
+      onClick={handleOnClick}
+    >
       {"Next: " + buttonPlaceholder}
     </button>
   );
-}
 
 
+
+
+
+
+
+
+
+
+  }
+  
