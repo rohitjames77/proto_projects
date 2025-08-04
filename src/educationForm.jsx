694965eb5cf3cf07bb49e21d71ffda1cea6f9ad6
Your Name <you@example.com>
2025-08-04 21:21:41 +0530

    Modularize each form logic

---
+++ src/educationForm.jsx
@@ -0,0 +1,105 @@
+
+
+
+
+export default function EducationForm({ personalInfo,formId,setFormId,idArr,educationInfo, setEducationInfo }) {
+  const [addfield, setAddField] = useState(false);
+
+
+  const handleOnclick = function () {
+    setAddField(true);
+  };
+
+  if (addfield) {
+      <ExtraEducationField
+      educationInfo= {educationInfo} 
+      setEducationInfo={setEducationInfo}
+      />
+    setAddField(false);
+  }
+
+  return (
+    <div className="" id="education-form-container">
+      <ExtraEducationField
+      educationInfo= {educationInfo} 
+      setEducationInfo={setEducationInfo}
+      />
+      <button
+        type="button"
+        className=""
+        id="add-education-field"
+        onClick={handleOnclick}
+      >Extra</button>
+
+        <FormForwardButton formId ={formId}
+      setFormId={setFormId}
+      idArr={idArr}
+    
+       />
+        <Test personalInfo={personalInfo} 
+      educationInfo={educationInfo}
+      />
+    </div>
+  );
+}
+
+function ExtraEducationField({educationInfo,setEducationInfo}) {
+const handleOnChange = function (event) {
+    const { id, value } = event.target;
+    setEducationInfo({ ...educationInfo, [id]: value });
+    
+  };
+  
+  console.log(educationInfo.school);
+
+
+  return (
+    <div className="bg-gray-200 flex-row flex-auto 10v h-40v w-2/3 ">
+      <label htmlFor="school" className="text-gray-800">School </label>
+      <input
+        type="text"
+        className="bg-gray-400 text-gray-800"
+        id="school"
+        value={educationInfo.school}
+        maxLength={50}
+        onChange={handleOnChange}
+        required
+      ></input>
+      <label htmlFor="university" className="text-gray-800" >University </label>
+      <input
+        type="text"
+        className="bg-gray-400 text-gray-800"
+        id="university"
+        value={educationInfo.university}
+        maxLength={50}
+        onChange={handleOnChange}
+        required
+      ></input>
+      <label htmlFor="degree-name" className="text-gray-800">Degree</label>
+      <input
+        type="text"
+        className="bg-gray-400 text-gray-800"
+        id="degreeName"
+        value={educationInfo.degree}
+        maxLength={50}
+        onChange={handleOnChange}
+        required
+      ></input>
+      <label htmlFor="start-date" className="text-gray-800">Start Date</label>
+      <input type="date" className="bg-gray-200" id="startDate" value={educationInfo.startDate} onChange={handleOnChange} required></input>
+      <label htmlFor="end-date" className="text-gray-800">End Date</label>
+      <input type="date" className="bg-gray-200" id="endDate" value={educationInfo.endDate}  onChange={handleOnChange}required></input>
+      <label htmlFor="location" className="text-gray-800">Location</label>
+      <input
+        type="text"
+        className="bg-gray-200"
+        id="location"
+        value={educationInfo.location}
+        maxLength={40}
+        onChange={handleOnChange}
+        required
+      ></input>
+     
+    </div>
+  );
+}
---
+++ src/personalForm.jsx
@@ -0,0 +1,99 @@
+
+
+export default function PersonalForm({ personalInfo, setPersonalInfo }) {
+  const handleOnChange = function (event) {
+    const { id, value } = event.target;
+    setPersonalInfo({ ...personalInfo, [id]: value });
+  };
+
+  return (
+    <div className="" id="personal-form-container">
+      <form className="" id="personal-form">
+        <label htmlFor="job-title">Job Title</label>
+        <input
+          type="text"
+          maxLength={50}
+          className=""
+          id="jobTitle"
+          value={personalInfo.jobTitle}
+          onChange={handleOnChange}
+          required
+        />
+
+        <label htmlFor="first-name">First Name</label>
+        <input
+          type="text"
+          maxLength={30}
+          className=""
+          id="firstName"
+          value={personalInfo.firstName}
+          onChange={handleOnChange}
+          required
+        />
+
+        <label htmlFor="last-name">Last Name</label>
+        <input
+          type="text"
+          maxLength={30}
+          className=""
+          id="lastName"
+          value={personalInfo.lastName}
+          onChange={handleOnChange}
+          required
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          className=""
+          id="email"
+          value={personalInfo.email}
+          onChange={handleOnChange}
+          required
+        />
+
+        <label htmlFor="phone">Phone</label>
+        <input
+          type="tel"
+          className=""
+          id="phone"
+          value={personalInfo.phone}
+          onChange={handleOnChange}
+        />
+
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          className=""
+          maxLength={100}
+          id="address"
+          value={personalInfo.address}
+          onChange={handleOnChange}
+          required
+        />
+
+        <label htmlFor="city">City</label>
+        <input
+          type="text"
+          className=""
+          maxLength={60}
+          id="city"
+          value={personalInfo.city}
+          onChange={handleOnChange}
+          required
+        />
+
+        <label htmlFor="country">Country</label>
+        <input
+          type="text"
+          className=""
+          maxLength={60}
+          id="country"
+          value={personalInfo.country}
+          onChange={handleOnChange}
+          required
+        />
+      </form>
+    </div>
+  );
+}
