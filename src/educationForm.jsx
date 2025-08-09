4067a39b8a6641bd7fcfec310728cdac0c1e92a3
Your Name <you@example.com>
2025-08-09 23:22:08 +0530

    Create logic for creating state object properties dynamically onClick of extra field button

--- src/educationForm.jsx
+++ src/educationForm.jsx
@@ -1,61 +1,151 @@
 import { useState } from "react";
+import FormForwardButton from "./changeForm";
+import ExtraEducationField2 from "./extraEduField2";
+import ExtraEducationField3 from "./extraEduField3";
 
+export default function EducationForm({
 
+  formId,
+  setFormId,
+  idArr,
+  educationInfo,
+  setEducationInfo,
+}) {
 
-export default function EducationForm({ personalInfo,formId,setFormId,idArr,educationInfo, setEducationInfo }) {
-  const [addfield, setAddField] = useState(false);
-
-
-  const handleOnclick = function () {
-    setAddField(true);
+  const [clickstatus,setClickStatus]=useState(false);
+  const handleOnChange = function (event) {
+    const { id, value } = event.target;
+    setEducationInfo({ ...educationInfo, [id]: value });
   };
 
-  if (addfield) {
-      <ExtraEducationField
-      educationInfo= {educationInfo} 
-      setEducationInfo={setEducationInfo}
-      />
-    setAddField(false);
-  }
+  const handleOnclick = function () {
+    setClickStatus(true)
+    if (clickstatus){
+    setEducationInfo(
+      Object.defineProperties(educationInfo, {
+        school2: { value: "" },
+        school3: { value: "" },
+        university2: { value: "" },
+        university3: { value: "" },
+        city2: { value: "" },
+        city3: { value: "" },
+        startDate2: { value: "" },
+        startDate3: { value: "" },
+        endDate2: { value: "" },
+        endDate3: { value: "" },
+        location2: { value: "" },
+        location3: { value: "" },
+        degree2: { value: "" },
+        degree3: { value: "" },
+      })
+    );
+    console.log(educationInfo);
+    
+    // return (
+    //   <div className="" id="education-form-container">
+    //     <label htmlFor="school" className="text-gray-800">
+    //       School
+    //     </label>
+    //     <input
+    //       type="text"
+    //       className="bg-gray-400 text-gray-800"
+    //       id="school"
+    //       value={educationInfo.school}
+    //       maxLength={50}
+    //       onChange={handleOnChange}
+    //       required
+    //     ></input>
+    //     <label htmlFor="university" className="text-gray-800">
+    //       University
+    //     </label>
+    //     <input
+    //       type="text"
+    //       className="bg-gray-400 text-gray-800"
+    //       id="university"
+    //       value={educationInfo.university}
+    //       maxLength={50}
+    //       onChange={handleOnChange}
+    //       required
+    //     ></input>
+    //     <label htmlFor="degree-name" className="text-gray-800">
+    //       Degree
+    //     </label>
+    //     <input
+    //       type="text"
+    //       className="bg-gray-400 text-gray-800"
+    //       id="degreeName"
+    //       value={educationInfo.degree}
+    //       maxLength={50}
+    //       onChange={handleOnChange}
+    //       required
+    //     ></input>
+    //     <label htmlFor="start-date" className="text-gray-800">
+    //       Start Date
+    //     </label>
+    //     <input
+    //       type="date"
+    //       className="bg-gray-200"
+    //       id="startDate"
+    //       value={educationInfo.startDate}
+    //       onChange={handleOnChange}
+    //       required
+    //     ></input>
+    //     <label htmlFor="end-date" className="text-gray-800">
+    //       End Date
+    //     </label>
+    //     <input
+    //       type="date"
+    //       className="bg-gray-200"
+    //       id="endDate"
+    //       value={educationInfo.endDate}
+    //       onChange={handleOnChange}
+    //       required
+    //     ></input>
+    //     <label htmlFor="location" className="text-gray-800">
+    //       Location
+    //     </label>
+    //     <input
+    //       type="text"
+    //       className="bg-gray-200"
+    //       id="location"
+    //       value={educationInfo.location}
+    //       maxLength={40}
+    //       onChange={handleOnChange}
+    //       required
+    //     ></input>
+    //     <button
+    //       type="button"
+    //       className=""
+    //       id="add-education-field"
+    //       onClick={handleOnclick}
+    //     >
+    //       Extra
+    //     </button>
+
+    //     <FormForwardButton
+    //       formId={formId}
+    //       setFormId={setFormId}
+    //       idArr={idArr}
+    //     />
+    //     <ExtraEducationField2
+    //       educationInfo={educationInfo}
+    //       setEducationInfo={setEducationInfo}
+    //     />
+    //     <ExtraEducationField3
+    //       educationInfo={educationInfo}
+    //       setEducationInfo={setEducationInfo}
+    //     />
+    //   </div>
+    //  )
+    }
+    
+    };
 
   return (
     <div className="" id="education-form-container">
-      <ExtraEducationField
-      educationInfo= {educationInfo} 
-      setEducationInfo={setEducationInfo}
-      />
-      <button
-        type="button"
-        className=""
-        id="add-education-field"
-        onClick={handleOnclick}
-      >Extra</button>
-
-        <FormForwardButton formId ={formId}
-      setFormId={setFormId}
-      idArr={idArr}
-    
-       />
-        <Test personalInfo={personalInfo} 
-      educationInfo={educationInfo}
-      />
-    </div>
-  );
-}
-
-function ExtraEducationField({educationInfo,setEducationInfo}) {
-const handleOnChange = function (event) {
-    const { id, value } = event.target;
-    setEducationInfo({ ...educationInfo, [id]: value });
-    
-  };
-  
-  console.log(educationInfo.school);
-
-
-  return (
-    <div className="bg-gray-200 flex-row flex-auto 10v h-40v w-2/3 ">
-      <label htmlFor="school" className="text-gray-800">School </label>
+      <label htmlFor="school" className="text-gray-50">
+        School
+      </label>
       <input
         type="text"
         className="bg-gray-400 text-gray-800"
@@ -65,17 +155,21 @@ const handleOnChange = function (event) {
         onChange={handleOnChange}
         required
       ></input>
-      <label htmlFor="university" className="text-gray-800" >University </label>
+      <label htmlFor="university" className="text-gray-50">
+        University
+      </label>
       <input
         type="text"
-        className="bg-gray-400 text-gray-800"
+        className="bg-gray-400 text-gray-50"
         id="university"
         value={educationInfo.university}
         maxLength={50}
         onChange={handleOnChange}
         required
       ></input>
-      <label htmlFor="degree-name" className="text-gray-800">Degree</label>
+      <label htmlFor="degree-name" className="text-gray-50">
+        Degree
+      </label>
       <input
         type="text"
         className="bg-gray-400 text-gray-800"
@@ -85,11 +179,31 @@ const handleOnChange = function (event) {
         onChange={handleOnChange}
         required
       ></input>
-      <label htmlFor="start-date" className="text-gray-800">Start Date</label>
-      <input type="date" className="bg-gray-200" id="startDate" value={educationInfo.startDate} onChange={handleOnChange} required></input>
-      <label htmlFor="end-date" className="text-gray-800">End Date</label>
-      <input type="date" className="bg-gray-200" id="endDate" value={educationInfo.endDate}  onChange={handleOnChange}required></input>
-      <label htmlFor="location" className="text-gray-800">Location</label>
+      <label htmlFor="start-date" className="text-gray-50">
+        Start Date
+      </label>
+      <input
+        type="date"
+        className="bg-gray-200"
+        id="startDate"
+        value={educationInfo.startDate}
+        onChange={handleOnChange}
+        required
+      ></input>
+      <label htmlFor="end-date" className="text-gray-50">
+        End Date
+      </label>
+      <input
+        type="date"
+        className="bg-gray-200"
+        id="endDate"
+        value={educationInfo.endDate}
+        onChange={handleOnChange}
+        required
+      ></input>
+      <label htmlFor="location" className="text-gray-50">
+        Location
+      </label>
       <input
         type="text"
         className="bg-gray-200"
@@ -99,7 +213,16 @@ const handleOnChange = function (event) {
         onChange={handleOnChange}
         required
       ></input>
-     
+      <button
+        type="button"
+        className=""
+        id="add-education-field"
+        onClick={handleOnclick}
+      >
+        Extra
+      </button>
+
+      <FormForwardButton formId={formId} setFormId={setFormId} idArr={idArr} />
     </div>
   );
 }
