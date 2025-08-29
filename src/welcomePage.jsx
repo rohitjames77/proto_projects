ed091389fd91d7294654d61701bc8ea6b16d7978
Your Name <you@example.com>
2025-08-29 08:38:20 +0530

    Remove border from welcome page

--- src/welcomePage.jsx
+++ src/welcomePage.jsx
@@ -17,10 +17,10 @@ export default function WelcomePage() {
 
   return (
     <div id="welcome-page-container" className=" w-screen h-screen bg-gradient-to-r from-indigo-400 to-violet-400">
-     <div id="intro-container" className="absolute top-100 left-60 h-[30vh] w-[70vw] border-solid border-3 border-red-50">
+     <div id="intro-container" className="absolute top-100 left-60 h-[30vh] w-[70vw] ">
       <p className="text-gray-50 text-5xl">Welcome to Resume Maker</p>
       <p className="text-gray-50 text-6xl"> Your first Step to Dream Career</p>
-      <button type="button" className="text-gray-50 bg-gradient-to-r from-red-400 to-orange-400 shadow-red-500 " onClick={handleOnclick}>
+      <button type="button" className="h-[5vh] w-[10vh ]text-gray-50 bg-gradient-to-r from-red-400 to-orange-400 shadow-red-500 " onClick={handleOnclick}>
         Let's Get Started
       </button>
       </div>
